a2b2ab806221148537068f2d6cc1414e9f06ad73
Ckai <ckai.trans@gmail.com>
2023-02-14 00:34:14 +0800

    65 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -24,9 +25,13 @@ const App = () => {
     },
   ];
 
+const App = () => {
+  const [ expenses, setExpenses ] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expenses);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
   }
 
   return (
